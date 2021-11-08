11de151db4b49dfdf605e0bbea811dd46b9a71c9
mitasu <osamutotsuki@gmail.com>
2021-11-08 16:34:39 +0900

    NAME.cs作成

---
+++ Assets/Scripts/NAME.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NAME : MonoBehaviour
+{
+    /**
+     ここにみんなの名前書いてみてね
+     ツヅキ
+     
+     
+     
+     
+     */
+}
